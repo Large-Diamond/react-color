494fe5a96e5ea1bd1a7ed1679663b621c02ebe1d
Banker523 <winner.ck523@gmail.com>
2022-02-07 14:24:13 +0800

    feat: update contracts to latest version

--- status-message-rs/src/lib.rs
+++ status-message-rs/src/lib.rs
@@ -1,30 +1,34 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::LookupMap;
-use near_sdk::{env, near_bindgen, AccountId};
+use near_sdk::{env, log, metadata, near_bindgen, AccountId};
 
-#[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
-pub struct StatusMessage {
-    records: LookupMap<AccountId, String>,
-}
+metadata!{
+    #[near_bindgen]
+    #[derive(BorshDeserialize, BorshSerialize)]
+    pub struct StatusMessage {
+        records: LookupMap<AccountId, String>,
+    }
 
-impl Default for StatusMessage {
-    fn default() -> Self {
-        Self {
-            records: LookupMap::new(b"r".to_vec()),
+    impl Default for StatusMessage {
+        fn default() -> Self {
+            Self {
+                records: LookupMap::new(b"r".to_vec()),
+            }
         }
     }
-}
 
-#[near_bindgen]
-impl StatusMessage {
-    pub fn set_status(&mut self, message: String) {
-        let account_id = env::signer_account_id();
-        self.records.insert(&account_id, &message);
-    }
+    #[near_bindgen]
+    impl StatusMessage {
+        pub fn set_status(&mut self, message: String) {
+            let account_id = env::signer_account_id();
+            log!("{} set_status with message {}", account_id, message);
+            self.records.insert(&account_id, &message);
+        }
 
-    pub fn get_status(&self, account_id: AccountId) -> Option<String> {
-        self.records.get(&account_id)
+        pub fn get_status(&self, account_id: AccountId) -> Option::<String> {
+            log!("get_status for account_id {}", account_id);
+            self.records.get(&account_id)
+        }
     }
 }
 
@@ -32,46 +36,35 @@ impl StatusMessage {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use near_sdk::test_utils::{get_logs, VMContextBuilder};
     use near_sdk::{testing_env, VMContext};
 
-    fn get_context(input: Vec<u8>) -> VMContext {
-        VMContext {
-            current_account_id: "alice_near".parse().unwrap(),
-            signer_account_id: "bob_near".parse().unwrap(),
-            signer_account_pk: vec![0, 1, 2],
-            predecessor_account_id: "carol_near".parse().unwrap(),
-            input,
-            block_index: 0,
-            block_timestamp: 0,
-            account_balance: 0,
-            account_locked_balance: 0,
-            storage_usage: 0,
-            attached_deposit: 0,
-            prepaid_gas: 10u64.pow(18),
-            random_seed: vec![0, 1, 2],
-            view_config: None,
-            output_data_receivers: vec![],
-            epoch_height: 0,
-        }
+    fn get_context(is_view: bool) -> VMContext {
+        VMContextBuilder::new()
+            .signer_account_id("bob_near".parse().unwrap())
+            .is_view(is_view)
+            .build()
     }
 
     #[test]
     fn set_get_message() {
-        let context = get_context(vec![]);
+        let context = get_context(false);
         testing_env!(context);
         let mut contract = StatusMessage::default();
         contract.set_status("hello".to_string());
-        assert_eq!(
-            "hello".to_string(),
-            contract.get_status("bob_near".parse().unwrap()).unwrap()
-        );
+        assert_eq!(get_logs(), vec!["bob_near set_status with message hello"]);
+        let context = get_context(true);
+        testing_env!(context);
+        assert_eq!("hello".to_string(), contract.get_status("bob_near".parse().unwrap()).unwrap());
+        assert_eq!(get_logs(), vec!["get_status for account_id bob_near"]);
     }
 
     #[test]
     fn get_nonexistent_message() {
-        let context = get_context(vec![]);
+        let context = get_context(true);
         testing_env!(context);
         let contract = StatusMessage::default();
         assert_eq!(None, contract.get_status("francis.near".parse().unwrap()));
+        assert_eq!(get_logs(), vec!["get_status for account_id francis.near"]);
     }
 }
